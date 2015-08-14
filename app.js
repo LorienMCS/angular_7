42e28a3591a9a4ab065e1f25d877c579faf3240a
Lorien <LorienMCS@gmail.com>
2015-08-13 18:17:17 -0700

    add some post functionality

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var app = angular.module("partSevenApp", ['ui.bootstrap']);
 
 app.controller("RedditClone", function($scope){
+	$scope.postForm = false;
+	$scope.posts = [];
+	$scope.post = {};
 
+	$scope.showPostForm = function() {
+		$scope.postForm = true;
+	};
+
+	$scope.addPost = function() {
+		$scope.posts.push($scope.post);
+		$scope.post = {};
+		$scope.postForm = false;
+	};
 
 })
